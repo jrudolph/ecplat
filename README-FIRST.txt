0d518429aeb6ed77fdd1c71d57d5fe02908dc764
Robert Simmons <robert@mosoro.com>
2013-02-08 23:53:18 -0700

    Added new dependencies for Camel-jetty Reworked starting Actors to resemble more the direction of the design Changed the actors to use Camel

--- README-FIRST.txt
+++ README-FIRST.txt
@@ -1,14 +1,23 @@
 Overview:
-The ecplat project is a demonstration of how to use Akka, Scala, Ajax and Gradle to build 
+The Ecplat project is a demonstration of how to use Akka, Scala, Ajax and Gradle to build
 a fully functional eCommerce platform. As the development of the example goes on the repository
 will be tagged with important versions that add functionality to the system. 
 
 Release Notes:
-Version: 0.0.1    Git Tag: hello-ecplat
+Version: 0.1    Git Tag: hello-ecplat
+  * Contributing Authors: Robert Simmons <kraythe@kraythensoft.com>
   * Requires only Gradle 1.4 (everything else will be pulled by gradle)
   * Very basic getting started with Akka and Gradle
   * To use type at the command prompt: gradle build run
   * The system will send two messages and then exit
-  * Contributing Authors: Robert Simmons <kraythe@kraythensoft.com>
 
+
+Version: 0.2    Git Tag: ecplat_with_camel-jetty
+  * Contributing Authors: Robert Simmons <kraythe@kraythensoft.com>
+  * Added new dependencies for Camel-jetty
+  * Reworked starting Actors to resemble more the direction of the design
+  * Changed the actors to use Camel
+  * To run the microkernel server use "gradle run" or run the 'com.kraythensoft.store.Main' class in your IDE
+  * You can send a message to the server with curl like this:
+    curl --data "name=Robert" http://localhost:8877/ecplat
 
--- src/main/scala/com/kraythensoft/ecplat/Actors.scala
+++ src/main/scala/com/kraythensoft/ecplat/Actors.scala
@@ -1,23 +1,32 @@
-package org.kraythensoft.store
+package com.kraythensoft.ecplat
 
-import akka.actor.{Props, ActorSystem, Actor}
+import akka.actor.{Props, ActorSystem}
 import akka.event.Logging
+import akka.camel.{CamelExtension, Consumer, CamelMessage}
+import scala.concurrent.duration._
+import org.apache.camel.component.jetty.JettyHttpComponent
+
 
 /** A general actor that will respond to simple text messages. */
-class MyActor extends Actor {
-  val log = Logging(context.system, this);
+class MainActor extends Consumer {
+  val log = Logging(context.system, this)
+
+  def endpointUri = "jetty:http://localhost:8877/ecplat"
 
   def receive = {
-    case "test" => log.info("received test")
-    case _ => log.info("received unknown message")
+    case msg: CamelMessage => {
+      log.info("Recieved Message from user: " + msg.toString())
+      sender ! ("You sent me the following data: %s" format msg.bodyAs[String])
+    }
   }
 }
 
 /** Application that runs the server. */
 object Main extends App {
   val system = ActorSystem()
-  val myActor = system.actorOf(Props[MyActor], name = "myactor")
-  myActor ! "foo"
-  myActor ! "test"
-  system.shutdown
+  val camel = CamelExtension(system)
+  val jetty = new JettyHttpComponent()
+  camel.context.addComponent("jetty", jetty)
+  val mainActor = system.actorOf(Props[MainActor], name = "MainActor")
+  val activationFuture = camel.activationFutureFor(mainActor)(timeout = 10 seconds, executor = system.dispatcher)
 }
